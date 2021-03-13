ace773d56e39747ff62f4bfb6633533c4ca925ae
Bienroth <79250095+Bienroth@users.noreply.github.com>
2021-03-13 10:24:29 +0100

    Tooltips Hover

---
+++ 802/Assets/Scripts/VRScripts/HoverManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverManager : MonoBehaviour
+{
+    public GameObject infoText;
+    private bool currentOn=false;
+    
+    public void enableText()
+    {
+        currentOn = true;
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    public void disableText()
+    {
+        currentOn = false;
+        infoText.SetActive(false);
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(1);
+        if(currentOn == true) infoText.SetActive(true);
+
+    }
+
+    
+}
--- 802/Assets/Scripts/VRScripts/UIHandlerWorkaround.cs
+++ 802/Assets/Scripts/VRScripts/UIHandlerWorkaround.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class UIHandlerWorkaround : MonoBehaviour
 {
     public GameObject sphere;
     void Update()
