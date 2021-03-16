917e2f395cad1a7bac9d8571c7871722e071d4e2
Bienroth <79250095+Bienroth@users.noreply.github.com>
2021-03-16 18:41:02 +0100

    LogFile

--- 802/Assets/Scripts/VRScripts/LogFile.cs
+++ 802/Assets/Scripts/VRScripts/LogFile.cs
@@ -6,14 +6,15 @@ using System.IO;
 public class LogFile : MonoBehaviour
 {
     private string path;
-
+    private bool save =true;
     private void Start()
     {
         path = Application.dataPath + "/SessionLog.txt";
-        File.WriteAllText(path, "Logfile \n\n");
-        File.AppendAllText(path, "Time of session start: " + System.DateTime.Now + "\n \n");
-        File.AppendAllText(path, "*********************************************************\n\n");
+           File.WriteAllText(path, "Logfile \n\n" + System.DateTime.Now + "\n \n");
+           File.AppendAllText(path, "*********************************************************\n\n");
 
+           File.AppendAllText(path, "Main Gene selected \t\t\t\t Compared with \t\t\t\t Timestamp \n");
+        File.AppendAllText(path, "_________________________________________________________________________________\n\n");
     }
 
 
@@ -22,8 +23,29 @@ public class LogFile : MonoBehaviour
     {
         gene = gene.Replace("<i>", "");
         gene = gene.Replace("</i>", "");
-        if (copy) File.AppendAllText(path, gene + " selected at: " + System.DateTime.Now.ToString("HH:MM:ss") + " for the combined view model. \n") ;
-        if (!copy) File.AppendAllText(path, gene + " selected at: " + System.DateTime.Now.ToString("HH:MM:ss") + " for the original model. \n");
 
+        if (save && copy)
+        {
+            //both copies
+            File.AppendAllText(path, "-----\t\t\t\t" +gene+ "\t\t\t\t" + System.DateTime.Now.ToString("HH:MM:ss") + "\n");
+
+        }
+        if (!save && !copy)
+        {
+            //both are original 
+            File.AppendAllText(path, "\t\t\t\t------- \t\t\t\t" + System.DateTime.Now.ToString("HH:MM:ss") +"\n");
+            File.AppendAllText(path, gene);
+        }
+        if (!save && copy)
+        {
+            //last copy new original 
+            File.AppendAllText(path, "\t\t\t\t" + gene + "\t\t\t\t" + System.DateTime.Now.ToString("HH:MM:ss") + "\n");
+        }
+        if (save && !copy)
+        {
+            //last copy new original 
+            File.AppendAllText(path, gene);
+        }
+        save = copy;
     }
 }
